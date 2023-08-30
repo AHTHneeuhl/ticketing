b1a98eac7d9f16eb881d1e0db251bc27f45fcbe0
Shubham Patrick <ahthneeuhl@gmail.com>
2023-08-30 11:17:54 +0530

    chore: scaffolding routes

--- auth/src/routes/currentUser.ts
+++ auth/src/routes/currentUser.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("/api/users/currentUser", () => {});
+router.get("/api/users/currentUser", (req, res) => {
+  res.send("Hi there!");
+});
 
 export { router as currentUserRouter };
