3bd3253de568e6e80be324046f36ba556505a7fa
Shubham Patrick <ahthneeuhl@gmail.com>
2023-08-30 11:32:15 +0530

    feat: add validation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,24 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
+import { body } from "express-validator";
 
 const router = Router();
 
-router.post("/api/users/signup", (req, res) => {
-  res.send("Hi there!");
-});
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 6, max: 20 })
+      .withMessage("Password must be between 6 and 20 characters"),
+  ],
+  (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    if (!email || typeof email !== "string") {
+      res.status(400).send("Provide a valid email");
+    }
+  }
+);
 
 export { router as signUpRouter };
