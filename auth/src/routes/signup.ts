9e53fd53d21564982ee64f4a79378219d7de2ba3
Shubham Patrick <ahthneeuhl@gmail.com>
2023-08-30 11:41:30 +0530

    feat: handle validation errors

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from "express";
-import { body } from "express-validator";
+import { body, validationResult } from "express-validator";
 
 const router = Router();
 
@@ -13,11 +13,13 @@ router.post(
       .withMessage("Password must be between 6 and 20 characters"),
   ],
   (req: Request, res: Response) => {
-    const { email, password } = req.body;
+    const errors = validationResult(req);
 
-    if (!email || typeof email !== "string") {
-      res.status(400).send("Provide a valid email");
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
     }
+
+    const { email, password } = req.body;
   }
 );
 
